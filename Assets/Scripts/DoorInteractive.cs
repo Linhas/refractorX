e90cb5779b73730187c2ff2b4afc230f5d8061ac
LuisTAC <luis.torrescosta@gmail.com>
2017-06-11 21:17:18 +0100

    Door opening on lever interaction on level_02

---
+++ Assets/Scripts/DoorInteractive.cs
@@ -0,0 +1,26 @@
+﻿using Interactive;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DoorInteractive : MonoBehaviour
+{
+
+    public GameObject Lever;
+
+    public Animator myAnimator;
+
+    // Use this for initialization
+    [UsedImplicitly]
+    private void Start()
+    {
+        LeverInteractive.OnStateChange += ChangeState;
+    }
+
+    private void ChangeState(int state, string name)
+    {
+        if (name == Lever.name)
+        {
+            myAnimator.SetBool("open", state==1);
+        }
+    }
+}
