eade8a9250cf92c49e824be10d029961e87826d2
PedroSama <futurewatertao@hotmail.com>
2017-06-12 12:23:39 +0100

    Doors can be controlled by multiple levers (◕‿◕✿)

---
+++ Assets/Scripts/DoorInteractiveMulti.cs
@@ -0,0 +1,30 @@
+﻿using Interactive;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class DoorInteractiveMulti : MonoBehaviour
+{
+
+    public GameObject[] Levers;
+
+    public Animator myAnimator;
+
+    // Use this for initialization
+    [UsedImplicitly]
+    private void Start()
+    {
+        LeverInteractive.OnStateChange += ChangeState;
+    }
+
+    private void ChangeState(int state, string name)
+    {
+        foreach (var lever in Levers)
+        {
+            if (name == lever.name)
+            {
+                myAnimator.SetBool("open", !myAnimator.GetBool("open"));
+            }
+        }
+       
+    }
+}
