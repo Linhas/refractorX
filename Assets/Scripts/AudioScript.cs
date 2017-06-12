9e257415c6e89fcaa5b8d8906bfe79157db3ee96
Linhas <ei11074@fe.up.pt>
2017-06-13 00:42:54 +0100

    More sounds, playing with lever animate.Sound not working yet

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    public AudioSource[] audios;
+
+    // Use this for initialization
+    void Start () {
+        AudioSource[] audios = GetComponents<AudioSource>();
+    }
+
+    void PlaySound()
+    {
+        audios[0].Play();
+    }
+	
+	
+}
