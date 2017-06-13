5878e356deebc1724513711eae47c4b6154c4bbc
Linhas <ei11074@fe.up.pt>
2017-06-13 02:20:09 +0100

    changed wav from ogg, walking animation should be working

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class AudioScript : MonoBehaviour {
 
-    public AudioSource[] audios;
+    public AudioClip[] audios;
 
     // Use this for initialization
     void Start () {
-        AudioSource[] audios = GetComponents<AudioSource>();
+        AudioSource audio = GetComponent<AudioSource>();
+        //AudioClip[] audios = GetComponents<AudioClip>();
     }
 
-    void PlaySound()
+    void PlaySound(int v)
     {
-        audios[0].Play();
+        GetComponent<AudioSource>().PlayOneShot(audios[v], 5);
+   //     audios[0].Play();
     }
-	
-	
+    void PlayFootsteps(int v)
+    {
+        GetComponent<AudioSource>().PlayOneShot(audios[Random.Range(1, audios.Length)], 1);
+        //     audios[0].Play();
+    }
+
+
+
 }
